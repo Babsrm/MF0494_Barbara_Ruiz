3b7c38ca725fa07db6afd670a0620d4a9cb842cc
Babsrm <babsruizm@gmail.com>
2023-02-16 11:57:27 +0100

    clase infractorDAO añadida

--- src/dao/InfractorDAO.java
+++ src/dao/InfractorDAO.java
@@ -25,7 +25,6 @@ public class InfractorDAO {
 
 
     public ArrayList<Infractor> obtenerInfractores() {
-    	// Obtenemos una conexion a la base de datos.
 		Connection con = conexion.getConexion();
 		Statement consulta = null;
 		ResultSet resultado = null;
@@ -48,7 +47,7 @@ public class InfractorDAO {
 			}
 			
 		} catch (SQLException e) {
-			System.out.println("Error al realizar la consulta sobre sanciones: "
+			System.out.println("Error al realizar la consulta sobre infractores: "
 					+e.getMessage());
 		} finally {
 			try {
@@ -64,68 +63,27 @@ public class InfractorDAO {
 		return lista;
     }
 
-    
-    
-
-    public Infractor obtenerInfractor(String dni) {
-		Connection con = conexion.getConexion();
-		PreparedStatement consulta = null;
-		ResultSet resultado = null;
-		Infractor infractor = null;
-		
-		try {
-			consulta = con.prepareStatement("select * from infractores"
-					+ " where dni = ?");
-			consulta.setInt(1, dni);
-			resultado=consulta.executeQuery();
-			
-			if (resultado.next()) {
-				String nombre = resultado.getString("nombre");
-				String apellidos = resultado.getString("apellidos");
-				int antiguedad = resultado.getInt("antiguedad");
-				float sancion = resultado.getFloat("sancion");
-				int puntos = resultado.getInt("puntos");
-				
-				infractor = new Infractor(dni, nombre, apellidos, antiguedad, sancion, puntos);
-				
-			}
-			
-		} catch (SQLException e) {
-			System.out.println("Error al realizar la consulta sobre una sancion: "
-		         +e.getMessage());
-		} finally {
-			try {
-				resultado.close();
-				consulta.close();
-				conexion.desconectar();
-			} catch (SQLException e) {
-				System.out.println("Error al liberar recursos: "+e.getMessage());
-			} catch (Exception e) {
-				
-			}
-		}
-		return s;
-    }
-
-
-    public int insertarSancion(Infractor s) {
-    	// Obtenemos una conexion a la base de datos.
+    public int insertarInfractor(Infractor infractor) {
 		Connection con = conexion.getConexion();
 		PreparedStatement consulta = null;
 		int resultado=0;
 		
 		try {
-			consulta = con.prepareStatement("INSERT INTO sanciones(descripcion, importe) \r\n"
-					+ "VALUES\r\n"
-					+ "(?,?)");
+			consulta = con.prepareStatement("INSERT INTO infractores(dni, nombre, apellidos, antiguedad, sancion, puntos) "
+					+ "VALUES "
+					+ "(?,?,?,?,?,?)");
 			
-			consulta.setString(1, s.getDescripcion());
-			consulta.setFloat(2, s.getImporte());
+			consulta.setString(1, infractor.getDni());
+			consulta.setString(2, infractor.getNombre());
+			consulta.setString(3, infractor.getApellidos());
+			consulta.setInt(4, infractor.getAntiguedad());
+			consulta.setFloat(5, infractor.getSancion());
+			consulta.setInt(6, infractor.getPuntos());
 			
 			resultado=consulta.executeUpdate();
 
 		} catch (SQLException e) {
-			System.out.println("Error al realizar la inserci�n de una sanci�n: "
+			System.out.println("Error al realizar la inserción de un infractor: "
 		        +e.getMessage());
 		} finally {
 			try {
@@ -139,29 +97,72 @@ public class InfractorDAO {
 		}
 		return resultado;
     }
+    
+    //Los siguientes métodos no los pide el ejercicio; se han añadido para futuras mejoras del proyecto.
+    public Infractor obtenerInfractor(String dni) {
+		Connection con = conexion.getConexion();
+		PreparedStatement consulta = null;
+		ResultSet resultado = null;
+		Infractor infractor = null;
+		
+		try {
+			consulta = con.prepareStatement("select * from infractores"
+					+ " where dni = ?");
+			consulta.setString(1, dni);
+			resultado=consulta.executeQuery();
+			
+			if (resultado.next()) {
+				String nombre = resultado.getString("nombre");
+				String apellidos = resultado.getString("apellidos");
+				int antiguedad = resultado.getInt("antiguedad");
+				float sancion = resultado.getFloat("sancion");
+				int puntos = resultado.getInt("puntos");
+				
+				infractor = new Infractor(dni, nombre, apellidos, antiguedad, sancion, puntos);			
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("Error al realizar la consulta sobre un infractor: "
+		         +e.getMessage());
+		} finally {
+			try {
+				resultado.close();
+				consulta.close();
+				conexion.desconectar();
+			} catch (SQLException e) {
+				System.out.println("Error al liberar recursos: "+e.getMessage());
+			} catch (Exception e) {
+				
+			}
+		}
+		return infractor;
+    }
 
-    public int actualizarSancion(Infractor s) {
-    	// Obtenemos una conexion a la base de datos.
+    public int actualizarInfractor(Infractor infractor) {
 		Connection con = conexion.getConexion();
 		PreparedStatement consulta = null;
 		int resultado=0;
 		
 		try {
-			consulta = con.prepareStatement("UPDATE sanciones\r\n"
-					+ "SET\r\n"
-					+ "descripcion = ?,\r\n"
-					+ "importe = ?,\r\n"
-					+ "WHERE idSancion = ?");
+			consulta = con.prepareStatement("UPDATE infractores "
+					+ "SET "
+					+ "nombre = ?, "
+					+ "apellidos = ?, "
+					+ "antiguedad = ?, "
+					+ "sancion = ?, "
+					+ "puntos = ?, "
+					+ "WHERE dni = ?");
 			
-
-			
-			consulta.setString(1, s.getDescripcion());
-			consulta.setFloat(2, s.getImporte());
-			consulta.setFloat(3, s.getIdSancion());
+			consulta.setString(6, infractor.getDni());
+			consulta.setString(1, infractor.getNombre());
+			consulta.setString(2, infractor.getApellidos());
+			consulta.setInt(3, infractor.getAntiguedad());
+			consulta.setFloat(4, infractor.getSancion());
+			consulta.setInt(5, infractor.getPuntos());
 			resultado=consulta.executeUpdate();
 
 		} catch (SQLException e) {
-			System.out.println("Error al realizar la actualizacion de la sanci�n: "
+			System.out.println("Error al realizar la actualizacion del infractor: "
 					+e.getMessage());
 		} finally {
 			try {
@@ -177,21 +178,20 @@ public class InfractorDAO {
     }
 
 
-    public int eliminarSancion(Infractor s) {
-    	// Obtenemos una conexion a la base de datos.
+    public int eliminarInfractor(Infractor infractor) {
 		Connection con = conexion.getConexion();
 		PreparedStatement consulta = null;
 		int resultado=0;
 		
 		try {
-			consulta = con.prepareStatement("DELETE FROM sanciones\r\n"
-					+ "WHERE idSancion = ?");
+			consulta = con.prepareStatement("DELETE FROM infractores "
+					+ "WHERE dni = ?");
 			
-			consulta.setInt(1, s.getIdSancion());
+			consulta.setString(1, infractor.getDni());
 			resultado=consulta.executeUpdate();
 
 		} catch (SQLException e) {
-			System.out.println("Error al realizar el borrado de una sanci�n: "+e.getMessage());
+			System.out.println("Error al realizar el borrado de un infractor: "+e.getMessage());
 		} finally {
 			try {
 				consulta.close();
